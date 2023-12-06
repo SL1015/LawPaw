978cd5050ef162462bde55786bdf62fe66786edc
SL1015 <91118605+SL1015@users.noreply.github.com>
2023-12-06 18:39:12 +0100

    Improve query logic and prompt context

--- backend/flaskapp/chatbot.py
+++ backend/flaskapp/chatbot.py
@@ -47,12 +47,14 @@ from transformers import AutoModel, AutoTokenizer
 import torch
 #device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+key_file = json.load(open("deepl_credential.json"))
+key_deepl =  key_file['key'][0]
+translator = deepl.Translator(key_deepl)
+
 tokenizer_en = AutoTokenizer.from_pretrained("thenlper/gte-small")
 model_en = AutoModel.from_pretrained("thenlper/gte-small")
 tokenizer_de = AutoTokenizer.from_pretrained("thenlper/gte-base")
 model_de = AutoModel.from_pretrained("thenlper/gte-base")
-
-
 
 from torch import Tensor
 def average_pool(last_hidden_states: Tensor,
@@ -83,50 +85,57 @@ def doc_to_rscope(docs):
   return rscope,links
 
 
-key_file = json.load(open("deepl_credential.json"))
-key_deepl =  key_file['key'][0]
-translator = deepl.Translator(key_deepl)
-
 def get_top_results(hits1,hits2):
   combined_lst = hits1 + hits2
   sorted_lst = sorted(combined_lst,key = lambda x:x.score,reverse=True)
   return sorted_lst[:3]
 
-def query_embedding(query,lang,kanton):
-    if kanton == 'all':
-        if lang != 'en' and lang != 'de':
-            print(lang)
-            query = translator.translate_text(query,target_lang='EN-GB').text
-            query_embedding = embedding(query,'en')
-        else:
-            query_embedding = embedding(query,lang)
+def query_embedding(query,lang,embedding_lang):
+    if lang != embedding_lang:
+        target_lang = 'EN-GB' if embedding_lang =='en' else 'DE'
+        query = translator.translate_text(query,target_lang=target_lang).text
+        current_app.logger.info("targetlang:"+ target_lang)
+        current_app.logger.info(query)
+        query_embedding = embedding(query,embedding_lang)
     else:
-        if lang != 'de':
-            query = translator.translate_text(query,target_lang='DE').text
-        query_embedding = embedding(query,'de')
+        query_embedding = embedding(query,lang)
     return query_embedding
 
-def search_context(query,lang,kanton):
-    query_vecs = query_embedding(query,lang,kanton)
+def search_context(query,lang,law,kanton):
+    embedding_en = query_embedding(query,lang,'en')
+    embedding_de = query_embedding(query,lang,'de')
     client = QdrantClient(host="qdrant",port=6333)
     if kanton != 'all':
-        collection1 = 'swiss-'+kanton
+        if law != 'OR':
+            query_vecs_kanton = embedding_de
+            query_vecs_gen = embedding_de
+            collection_canton = 'swiss-'+kanton
+            collection_gen = 'swiss-de'
+        else:    
+            query_vecs_kanton = embedding_de
+            query_vecs_gen = embedding_en
+            collection_canton = 'swiss-'+kanton
+            collection_gen = 'swiss-or'
+        current_app.logger.info(collection_gen)
         hits1=client.search(
-            collection_name=collection1,
-            query_vector = query_vecs,
+            collection_name=collection_canton,
+            query_vector = query_vecs_kanton,
             limit=3
         )
         hits2=client.search(
-            collection_name = 'swiss-de',
-            query_vector = query_vecs,
+            collection_name = collection_gen,
+            query_vector = query_vecs_gen,
             limit = 3
         )
         hits = get_top_results(hits1,hits2)
+            
     else:
-        if lang!='de':
+        if law =='OR':
             collection = 'swiss-or'
+            query_vecs = embedding_en
         else:
             collection = 'swiss-de'
+            query_vecs = embedding_de
         hits=client.search(
             collection_name=collection,
             query_vector = query_vecs,
@@ -138,17 +147,16 @@ import openai
 key_openai = json.load(open("openai_credential.json"))
 os.environ['OPENAI_API_KEY'] = key_openai['key'][0]
 
-def qa_chatbot(query, lang, kanton):
-  context_raw = search_context(query, lang, kanton)
+def qa_chatbot(query, lang, law, kanton):
+  context_raw = search_context(query, lang, law, kanton)
   current_app.logger.info(context_raw)
   context,links = doc_to_rscope(context_raw)
-  memory = ConversationBufferMemory(k=10,memory_key='chat_history')
+  memory = ConversationBufferMemory(k=10, memory_key='chat_history')
   ollama = ChatOllama(base_url='http://ollama:11434', model="mistral", temperature=0.1)
   chat_text = """
-  You are a legal assistant expert on the Swiss Code of Obligations.
-  Answer questions related to contract law, employment regulations,
-  or corporate obligations.
-  Base your answers exclusively on the provided top 3 articles from the Swiss Code of Obligations.
+  You are a legal assistant expert on the Swiss Private Law.
+  Answer questions related to contract law, employment regulations, corporate obligations, personality rights, family law (marriage, divorce, guardianship), inheritance law, and property law.
+  Base your answers exclusively on the provided top 3 articles from the law: {law}.
   Please provide a summary of the relevant article(s), along with the source link(s) for reference.
   The souce link(s) should be from the following collection {source_links}, if none of the links works, just don't provide the information.
   If an answer is not explicitly covered in the provided context, please indicate so by saying 'Whoopsie! It seems we took a detour from the legal zone. Let's hop back to law talk. Ask me anything about contracts, family law, or legal advice!'
@@ -160,7 +168,7 @@ def qa_chatbot(query, lang, kanton):
   chatgpt_llm_chain = LLMChain(prompt=prompt_template, llm=llm)
   ollama_llm_chain = LLMChain(prompt=prompt_template, llm=ollama)
   #answer = ollama_llm_chain.run(context=chat_text, query=query,source_links=links)
-  answer_gpt = chatgpt_llm_chain.run(context=chat_text, query=query, source_links=links)
+  answer_gpt = chatgpt_llm_chain.run(context=chat_text, query=query, source_links=links, law=law)
   if lang != ('en' or 'de'):
       detector = translator.translate_text(query, target_lang="DE")
       #answer = translator.translate_text(answer, target_lang=detector.detected_source_lang).text
@@ -192,11 +200,12 @@ def getResponse():
     data = request.get_json()
     user_message = data.get('message').strip('\"')
     lang = data.get('lang').strip('\"')
+    law = data.get('law').strip('\"')
     kanton = data.get('kanton').strip('\"')
     current_app.logger.info(lang)
     current_app.logger.info(kanton)
     # If there's no message provided, return an error message
     if not user_message:
         return jsonify({"error": "No message provided"}), 400
-    response = qa_chatbot(user_message, lang, kanton)
+    response = qa_chatbot(user_message, lang, law, kanton)
     return response
